643f4856a873c15e9346a0d7c28eac76e15317d1
Caio Teixeira - COAG <caio.teixeira@escoladc.asr.org.br>
2019-04-08 17:33:36 -0300

    fisrt commit

---
+++ precache-manifest.267525952c4797a2514608aad9bad500.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "035a9a0a01b49a7489c9",
+    "url": "/css/app.09d9f8dc.css"
+  },
+  {
+    "revision": "035a9a0a01b49a7489c9",
+    "url": "/js/app.2c9c6025.js"
+  },
+  {
+    "revision": "06f761203cfa376803a8",
+    "url": "/css/chunk-vendors.be8a8d68.css"
+  },
+  {
+    "revision": "06f761203cfa376803a8",
+    "url": "/js/chunk-vendors.a7a046e7.js"
+  },
+  {
+    "revision": "3c7895452dbf3a2b5ef69bc69d5c62ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a70f280abd7d35434c05a7a1be2e73f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "6e113c56651bcbb567e774af6f671789",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "645edc02c060b29064d3b722bb8cf6ea",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "f279c753513f3ba7ff6118e3adc51912",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "fdbfc0b7b290e15c0f316a5924310077",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "c9d7c6d2ff3ececbd1433a8b4b7245b4",
+    "url": "/images/piggy-bank.svg"
+  },
+  {
+    "revision": "4fb786f9852dc9dd9acf807ccbfdd202",
+    "url": "/images/icons/icon.svg"
+  },
+  {
+    "revision": "5f045f98dc47303f8886f77b8c2629d7",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "b512de109ab53c8646bedd9ec6a7ab42",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3677b6e82351308537180e1b2b669dd3",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "7e13d12f116cb216f9b017cd99825eb9",
+    "url": "/images/icons/icon-384x384.png"
+  }
+];
